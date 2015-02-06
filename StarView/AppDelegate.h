9e286b166e91370efa069dfe910274425c289db6
Yang <Yang@yanglingdeMac-mini.local>
2015-02-06 10:12:21 +0800

    Initial Commit

---
+++ StarView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarView
+//
+//  Created by Yang on 15/2/6.
+//  Copyright (c) 2015年 Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StarView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarView
+//
+//  Created by Yang on 15/2/6.
+//  Copyright (c) 2015年 Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
